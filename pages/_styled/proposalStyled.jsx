3db44312f85717894eefd41ccf226e2c91156098
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-28 01:23:33 +0900

    design: 건의하기 페이지 레이아웃

--- pages/_styled/proposalStyled.jsx
+++ pages/_styled/proposalStyled.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // 건의 컨테이너
 export const ReportContainer = styled.div`
-  margin: 0px 30px;
+  margin: 20px 30px;
 `;
 
 // 건의 헤더
@@ -20,4 +20,127 @@ export const ReportHeader = styled.div`
 export const ProposalFontAwesomeIcon = styled(FontAwesomeIcon)`
   color: #04c96b;
   width: 18px;
+`;
+
+// 건의 폼
+export const ReportForm = styled.form``;
+
+// 건의 문항별 헤더
+export const ReportOptionHeader = styled.div`
+  font-size: 18px;
+  text-align: left;
+  font-weight: 500;
+  margin: 30px 0px 17px 0px;
+  line-height: 1.3;
+  letter-spacing: -0.3px;
+`;
+
+export const ReportTextArea = styled.textarea`
+  width: 332px;
+  padding: 17px;
+  height: 160px;
+  align-items: flex-start;
+  display: flex;
+  border: 1px solid #b8b8b8;
+  border-radius: 10px;
+  line-height: 1.5;
+  resize: none;
+
+  .question {
+    font-size: 12px;
+    padding: 10px 17px;
+  }
+  ::placeholder {
+    color: #b8b8b8;
+    text-align: left;
+    padding-left: 10px;
+    padding: 0;
+    margin: 0;
+  }
+  &:focus {
+    outline: none;
+    border: 1px solid #04c96b;
+    box-shadow: 0 0 0 1px rgba(4, 201, 107, 0.3);
+  }
+`;
+
+export const ReportNoticeSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 10px;
+  text-align: left;
+  color: #04c96b;
+  padding: 3px 0;
+
+  li {
+    padding: 3px 0;
+    margin-left: 5px;
+    line-height: 14px;
+  }
+
+  .round {
+    border: 1px solid #04c96b;
+    width: 100px;
+    border-radius: 10px;
+    text-align: center;
+    padding: 4px;
+  }
+
+  .question {
+    color: #7a7a7a;
+    margin-bottom: 10px;
+    padding-bottom: 0;
+    font-size: 15px;
+  }
+`;
+export const ReportNoticeSectionSpan = styled.span`
+  color: #000;
+  margin-top: -8px;
+  line-height: 1.4px;
+`;
+
+export const ReportNoticeFontAwesomeIcon = styled(FontAwesomeIcon)`
+  width: 10px;
+`;
+
+export const RowInputBoxWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const RowInputBox = styled.input`
+  width: 332px;
+  padding: 17px;
+  align-items: flex-start;
+  display: flex;
+  border: 1px solid #b8b8b8;
+  border-radius: 10px;
+  line-height: 1.5;
+  margin-top: 22px;
+
+  ::placeholder {
+    color: #b8b8b8;
+    text-align: left;
+    padding-left: 10px;
+    padding: 0;
+    margin: 0;
+  }
+
+  &:focus {
+    outline: none;
+    border: 1px solid #04c96b;
+    box-shadow: 0 0 0 1px rgba(4, 201, 107, 0.3);
+  }
+`;
+
+// 제출 버튼
+export const SubmitButton = styled.button`
+  margin: 70px 0px 40px 0px;
+  background-color: #04c96b;
+  color: #fff;
+  padding: 15px 136.8px;
+  border: none;
+  border-radius: 14px;
+  cursor: pointer;
 `;
--- pages/proposal/index.jsx
+++ pages/proposal/index.jsx
@@ -1,16 +1,53 @@
 import * as S from "../_styled/proposalStyled";
 
 // 아이콘
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPaperPlane,
+  faCircleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Proposal() {
   return (
     <>
       <S.ReportContainer>
+        {/* 건의하기 헤더 */}
         <S.ReportHeader>
           <S.ProposalFontAwesomeIcon icon={faPaperPlane} />
           &nbsp;건의하기
         </S.ReportHeader>
+
+        {/* 건의하기 폼 작성 */}
+        <S.ReportForm>
+          <S.ReportOptionHeader>
+            1. 건의사항을 남겨주세요 *
+          </S.ReportOptionHeader>
+          <S.ReportTextArea
+            placeholder="텍스트를 입력해주세요."
+            // value={content}
+            // onChange={handleContentChange}
+            required
+          />
+
+          <S.ReportOptionHeader>
+            2. 답변을 받길 원하시면 이메일을 남겨주세요.(선택)
+          </S.ReportOptionHeader>
+          <S.ReportNoticeSection>
+            <S.ReportNoticeSectionSpan>
+              <S.ReportNoticeFontAwesomeIcon icon={faCircleExclamation} />
+              &nbsp;제보글, 댓글 삭제 요청일 시 이메일을 꼭 입력해주세요
+            </S.ReportNoticeSectionSpan>
+          </S.ReportNoticeSection>
+
+          <S.RowInputBoxWrap>
+            <S.RowInputBox
+              placeholder="elepahnt@dgu.ac.kr"
+              // value={contact}
+              type="email"
+              // onChange={handleContactChange}
+            ></S.RowInputBox>
+          </S.RowInputBoxWrap>
+          <S.SubmitButton type="submit">제출하기</S.SubmitButton>
+        </S.ReportForm>
       </S.ReportContainer>
     </>
   );
