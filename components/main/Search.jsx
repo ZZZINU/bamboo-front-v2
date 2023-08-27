4a337e98aec70800b7af56bce6a6c4b2a6647c92
Seulgigi <2021112414@dgu.ac.kr>
2023-08-27 23:57:15 +0900

    fix: 검색 컴포넌트 Input 태그로 수정

--- components/main/Search.jsx
+++ components/main/Search.jsx
@@ -1,18 +1,29 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { 
-  SearchBtn,
-  SearchContainer,
-  SearchText
-} from './Styled.jsx'
+import * as S from './Styled.jsx'
+import { useState } from 'react';
 
 export default function Search() {
+
+  const [search, setSearch] = useState('');
+  
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    console.log(search)
+  }
     
   return (
-    <SearchContainer>
-      <SearchText>
-        검색어를 입력해주세요
-      </SearchText>
-      <SearchBtn icon={faSearch} />
-    </SearchContainer>
+    <S.SearchContainer onSubmit={handleSubmit}>
+      <S.SearchText
+        placeholder='검색어를 입력해주세요'
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <S.SearchBtn>
+        <S.SearchFont icon={faSearch}/>
+      </S.SearchBtn>
+    </S.SearchContainer>
   )
 }
