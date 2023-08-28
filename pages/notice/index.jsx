c2d994528ebc3b8f7b7c81f7444e4e1ec203c617
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-08-28 17:19:40 +0900

    feat: 공지사항 페이지 - 페이지네이션 추가

--- pages/notice/index.jsx
+++ pages/notice/index.jsx
@@ -1,77 +1,100 @@
-import NoticeCard from '@/components/notice/NoticeCard';
-import * as S from '../_styled/noticeStyled'
+import NoticeCard from "@/components/notice/NoticeCard";
+import * as S from "../_styled/noticeStyled";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
+import Paging from "@/components/paging/Paging";
 
 export default function Notice() {
+  const [noticePosts, setNoticePosts] = useState([]);
 
-  const [noticePosts, setNoticePosts] = useState([])
-  const fetchNoticePosts = async() => {
-    try{
+  // 데이터 개수 count
+  const [count, setCount] = useState(0);
+
+  // 현재 페이지
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // 한 페이지당 보여줄 댓글 수
+  const itemsPerPage = 10;
+
+  // 페이지 변경 핸들러
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const fetchNoticePosts = async () => {
+    try {
       // dummy
       const dummy = [
-          {
-              id: 1,
-              title: "공지다",
-              content: "잠 좀 자라",
-              created_at: "2023-05-22T01:29:51",
-              images: [
-                // 걍 사진 가져옴
-                  "https://play-lh.googleusercontent.com/Kbu0747Cx3rpzHcSbtM1zDriGFG74zVbtkPmVnOKpmLCS59l7IuKD5M3MKbaq_nEaZM=w480-h960-rw",
-                  "https://play-lh.googleusercontent.com/Kbu0747Cx3rpzHcSbtM1zDriGFG74zVbtkPmVnOKpmLCS59l7IuKD5M3MKbaq_nEaZM=w480-h960-rw"
-              ]
-          },
-          {
-              id: 2,
-              title: "이미지없는공지다.",
-              content: "이미지없다고",
-              created_at: "2023-05-22T01:31:45",
-              images: []
-          },
-          {
-            id: 3,
-            title: "이미지없는공지다.",
-            content: "이미지없다고",
-            created_at: "2023-05-22T01:31:45",
-            images: []
-        }
+        {
+          id: 1,
+          title: "공지다",
+          content: "잠 좀 자라",
+          created_at: "2023-05-22T01:29:51",
+          images: [
+            // 걍 사진 가져옴
+            "https://play-lh.googleusercontent.com/Kbu0747Cx3rpzHcSbtM1zDriGFG74zVbtkPmVnOKpmLCS59l7IuKD5M3MKbaq_nEaZM=w480-h960-rw",
+            "https://play-lh.googleusercontent.com/Kbu0747Cx3rpzHcSbtM1zDriGFG74zVbtkPmVnOKpmLCS59l7IuKD5M3MKbaq_nEaZM=w480-h960-rw",
+          ],
+        },
+        {
+          id: 2,
+          title: "이미지없는공지다.",
+          content: "이미지없다고",
+          created_at: "2023-05-22T01:31:45",
+          images: [],
+        },
+        {
+          id: 3,
+          title: "이미지없는공지다.",
+          content: "이미지없다고",
+          created_at: "2023-05-22T01:31:45",
+          images: [],
+        },
       ];
 
-      setNoticePosts(dummy)
-      console.log(noticePosts[0].images[1])
-    } catch(e) {
-      console.log(e)
+      setNoticePosts(dummy);
+      setCount(3);
+      console.log(noticePosts[0].images[1]);
+    } catch (e) {
+      console.log(e);
     }
-  }
+  };
 
-  useEffect(()=>{
-    fetchNoticePosts()
-  }, [])
+  useEffect(() => {
+    fetchNoticePosts();
+  }, []);
 
   return (
-    <S.NoticeWrap>
+    <>
+      <S.NoticeWrap>
         <S.NoticeHeader>
-            <S.NoticeHeaderIcon icon={faBell} />
-            공지사항
+          <S.NoticeHeaderIcon icon={faBell} />
+          공지사항
         </S.NoticeHeader>
 
         <S.NoticePosts>
-          { noticePosts.length !=0 ? <>
-            {noticePosts.map((post)=>(
-              <S.NoticePost key={post.id}>
-                <NoticeCard
-                  post={post}
-                />
-              </S.NoticePost>
-            ))}
-          </> : <>
-            <S.NoticeNone>
-              등록된 공지가 없습니다.
-            </S.NoticeNone>
-          </> }
-          
+          {noticePosts.length != 0 ? (
+            <>
+              {noticePosts.map((post) => (
+                <S.NoticePost key={post.id}>
+                  <NoticeCard post={post} />
+                </S.NoticePost>
+              ))}
+            </>
+          ) : (
+            <>
+              <S.NoticeNone>등록된 공지가 없습니다.</S.NoticeNone>
+            </>
+          )}
         </S.NoticePosts>
-    </S.NoticeWrap>
-  )
+      </S.NoticeWrap>
+
+      <Paging
+        page={currentPage}
+        count={count}
+        postPerPage={itemsPerPage}
+        setPage={handlePageChange}
+      />
+    </>
+  );
 }
